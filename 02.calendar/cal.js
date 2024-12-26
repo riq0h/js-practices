128f5e237831b5c4832f5db9ff2fcf26311809f2
Rikuoh <mail@riq0h.jp>
2024-12-26 11:02:52 +0900

    月表示のフォーマットを修正、変数名の一貫性を向上

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -14,20 +14,20 @@ const today = new Date();
 const year = values.y ? parseInt(values.y) : today.getFullYear();
 const month = values.m ? parseInt(values.m) : today.getMonth() + 1;
 
-const firstDay = new Date(year, month - 1, 1);
-const lastDay = new Date(year, month, 0);
+const firstDate = new Date(year, month - 1, 1);
+const lastDate = new Date(year, month, 0);
 
-console.log(`${month.toString().padStart(7)} 月 ${year}`);
+console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
 
-stdout.write(" ".repeat(3 * firstDay.getDay()));
+stdout.write("   ".repeat(firstDate.getDay()));
 
 for (
-  let date = new Date(firstDay);
-  date <= lastDay;
+  let date = new Date(firstDate);
+  date <= lastDate;
   date.setDate(date.getDate() + 1)
 ) {
-  let dayString = date.getDate().toString().padStart(2, " ");
+  let dayString = date.getDate().toString().padStart(2);
 
   if (date.toDateString() === today.toDateString()) {
     dayString = `\x1b[7m${dayString}\x1b[0m`;

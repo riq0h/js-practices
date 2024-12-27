cd0d089278ff52f88170798fbe6d3cc777520b32
Rikuoh Tsujitani <mail@riq0h.jp>
2024-12-27 10:05:55 +0900

    参照機能の実装

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,6 +1,12 @@
 import { MemoManager } from "./src/MemoManager.js";
+import readline from "readline";
 
 const memoManager = new MemoManager();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 async function main() {
   await memoManager.init();
@@ -16,6 +22,17 @@ async function main() {
     console.log("メモが追加されました。");
   } else if (args[0] === "-l") {
     console.log(memoManager.listMemos().join("\n"));
+  } else if (args[0] === "-r") {
+    const memos = memoManager.listMemos();
+    for (let i = 0; i < memos.length; i++) {
+      console.log(`${i + 1}. ${memos[i]}`);
+    }
+    rl.question("表示したいメモの番号を選択してください: ", (answer) => {
+      const index = parseInt(answer) - 1;
+      const memo = memoManager.getMemo(index);
+      console.log(memo.content);
+      rl.close();
+    });
   }
 }
 

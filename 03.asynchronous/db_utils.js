6da9b12d78241c400bdd9d7dfebb6939bd8d75b0
Rikuoh <mail@riq0h.jp>
2025-01-11 09:49:46 +0900

    if文にブロックを追加

--- 03.asynchronous/db_utils.js
+++ 03.asynchronous/db_utils.js
@@ -3,8 +3,11 @@ import sqlite3 from "sqlite3";
 export function openDatabase(path) {
   return new Promise((resolve, reject) => {
     const db = new sqlite3.Database(path, (err) => {
-      if (err) reject(err);
-      else resolve(db);
+      if (err) {
+        reject(err);
+      } else {
+        resolve(db);
+      }
     });
   });
 }
@@ -12,8 +15,11 @@ export function openDatabase(path) {
 export function run(db, sql, params = []) {
   return new Promise((resolve, reject) => {
     db.run(sql, params, function (err) {
-      if (err) reject(err);
-      else resolve({ lastID: this.lastID, changes: this.changes });
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ lastID: this.lastID, changes: this.changes });
+      }
     });
   });
 }
@@ -21,8 +27,11 @@ export function run(db, sql, params = []) {
 export function get(db, sql, params = []) {
   return new Promise((resolve, reject) => {
     db.get(sql, params, (err, row) => {
-      if (err) reject(err);
-      else resolve(row);
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row);
+      }
     });
   });
 }
@@ -30,8 +39,11 @@ export function get(db, sql, params = []) {
 export function all(db, sql, params = []) {
   return new Promise((resolve, reject) => {
     db.all(sql, params, (err, rows) => {
-      if (err) reject(err);
-      else resolve(rows);
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
     });
   });
 }
@@ -39,8 +51,11 @@ export function all(db, sql, params = []) {
 export function close(db) {
   return new Promise((resolve, reject) => {
     db.close((err) => {
-      if (err) reject(err);
-      else resolve();
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
     });
   });
 }

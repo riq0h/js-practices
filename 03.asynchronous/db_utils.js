783b83922af5c075a9e612c7ff67dee8cd9f04a1
Rikuoh <mail@riq0h.jp>
2025-01-11 10:40:55 +0900

    デフォルト引数の削除

--- 03.asynchronous/db_utils.js
+++ 03.asynchronous/db_utils.js
@@ -12,7 +12,7 @@ export function openDatabase(path) {
   });
 }
 
-export function run(db, sql, params = []) {
+export function run(db, sql, params) {
   return new Promise((resolve, reject) => {
     db.run(sql, params, function (err) {
       if (err) {
@@ -24,19 +24,7 @@ export function run(db, sql, params = []) {
   });
 }
 
-export function get(db, sql, params = []) {
-  return new Promise((resolve, reject) => {
-    db.get(sql, params, (err, row) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(row);
-      }
-    });
-  });
-}
-
-export function all(db, sql, params = []) {
+export function all(db, sql, params) {
   return new Promise((resolve, reject) => {
     db.all(sql, params, (err, rows) => {
       if (err) {
